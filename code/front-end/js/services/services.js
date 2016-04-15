77db8af14603d3a8c64cd0b5058b7cbe3eb6350f
Tuan Anh Nguyen Ngoc <anh@divvit.com>
2016-04-16 00:40:33 +0700

    update json path to github

--- code/front-end/js/services/services.js
+++ code/front-end/js/services/services.js
@@ -4,7 +4,7 @@ angular.module('koginity').service('productService', function($timeout, $http) {
     var productService = {
         getList: function() {
             return $timeout(function() {
-                return $http.get('http://localhost/products.json').then(function(response) {
+                return $http.get('https://raw.githubusercontent.com/dauden/koginity-demo/master/code/front-end/data/products.json').then(function(response) {
                     return response.data;
                 });
             }, 30);
